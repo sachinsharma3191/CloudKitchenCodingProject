924500a5e44d31bf269cf2e3d815929681d4262f
sachinsharma <sachinsharma3191@gmail.com>
2024-06-23 19:08:15 -0700

    first commit

--- src/main/java/com/cloud/kitchen/util/JsonUtility.java
+++ src/main/java/com/cloud/kitchen/util/JsonUtility.java
@@ -1,23 +1,25 @@
 package com.cloud.kitchen.util;
 
-import java.util.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.DeserializationFeature ;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.core.type.TypeReference;
-import java.io.*;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 import com.cloud.kitchen.models.Order;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtility {
 
     private final static Logger logger = LogManager.getLogger(JsonUtility.class);
 
-    public static ObjectMapper getObjectMapper(){
+    public static ObjectMapper getObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
@@ -26,18 +28,18 @@ public class JsonUtility {
         return objectMapper;
     }
 
-    public static List<Order> readOrders(){
+    public static List<Order> readOrders() {
         List<Order> orders = new ArrayList<>();
         try {
             InputStream inputStream = JsonUtility.class.getClassLoader().getResourceAsStream(com.cloud.kitchen.constants.StringConstants.ORDERS_FILE);
-            if (inputStream == null) {
+            if(inputStream == null) {
                 logger.error("File not found!");
                 return orders;
             }
             orders = getObjectMapper().readValue(inputStream, new TypeReference<>() {
             });
-        }catch (Exception exception) {
-            logger.error("errorMessage={}. stackTrace={}",exception, ExceptionUtils.getMessage(exception));
+        } catch (Exception exception) {
+            logger.error("errorMessage={}. stackTrace={}", exception, ExceptionUtils.getMessage(exception));
         }
         return orders;
     }
